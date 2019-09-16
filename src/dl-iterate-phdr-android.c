fd1f122dfc9d5c17435dc5dcbb9b4c7db606cad9
Sim Sun <simsun@fb.com>
2019-09-16 14:57:37 -0700

    add implementation of dl_iterate_phdr on Android < 21 devices

--- src/dl-iterate-phdr-android.c
+++ src/dl-iterate-phdr-android.c
@@ -27,7 +27,6 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  */
 
 #include "libunwind_i.h"
 #include "os-linux.h"
-#include "dl-iterate-phdr.h"
 
 #ifndef IS_ELF
 /* Copied from NDK header. */
